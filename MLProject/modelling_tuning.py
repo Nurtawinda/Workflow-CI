b056953d0238c15c88b28e809ebd2979d276e3e2
WinWind <ntwind29@gmail.com>
2026-06-03 21:02:57 +0800

    deleted:    .vscode/settings.json 	modified:   MLProject/MLProject 	new file:   MLProject/conda.yaml 	modified:   MLProject/modelling_tuning.py

--- MLProject/modelling_tuning.py
+++ MLProject/modelling_tuning.py
@@ -4,48 +4,46 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import random
 import numpy as np
+import os
+import warnings
+import sys
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
 
-# Create a new MLflow Experiment
-mlflow.set_experiment("SML Winda")
-
-data = pd.read_csv(r"D:\winda's Project\Belajar Dicoding\Pijak By Dicoding\Submission\MSML\Workflow-CI\MLProject\credit_risk_dataset_preprocessing.csv")
-
-X_train, X_test, y_train, y_test = train_test_split(
-    data.drop("loan_status", axis=1),
-    data["loan_status"],
-    random_state=42,
-    test_size=0.2
-)
-input_example = X_train[0:5]
-# Mendifinisikan model menggunakan hyperparameter tuning.
-# Define Elastic Search parameters
-n_estimators_range = np.linspace(10, 1000, 5, dtype=int)  # 5 evenly spaced values
-max_depth_range = np.linspace(1, 50, 5, dtype=int)  # 5 evenly spaced values
-
-best_accuracy = 0
-best_params = {}
-
-for n_estimators in n_estimators_range:
-    for max_depth in max_depth_range:
-        with mlflow.start_run(run_name=f"elastic_search_{n_estimators}_{max_depth}"):
-            mlflow.autolog()
-
-            # Train model
-            model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
-            model.fit(X_train, y_train)
-
-            # Evaluate model
-            accuracy = model.score(X_test, y_test)
-            mlflow.log_metric("accuracy", accuracy)
-
-            # Save the best model
-            if accuracy > best_accuracy:
-                best_accuracy = accuracy
-                best_params = {"n_estimators": n_estimators, "max_depth": max_depth}
-                mlflow.sklearn.log_model(
-                    sk_model=model,
-                    artifact_path="model",
-                    input_example=input_example
-                    )
+    os.environ["MLFLOW_TRACKING_USERNAME"] = "Nurtawinda"
+    os.environ["MLFLOW_TRACKING_PASSWORD"] = "e6481f90867f23522774600ba572268d89071390"
+    DAGSHUB_MLFLOW_URI = "https://dagshub.com"
+    mlflow.set_tracking_uri(DAGSHUB_MLFLOW_URI)
+    mlflow.set_experiment("Submission SML winda")
+    
+    file_path = sys.argv[3] if len(sys.argv) > 3 else os.path.join(os.path.dirname(os.path.abspath(__file__)), "credit_risk_dataset_preprocessing.csv")
+    data = pd.read_csv(file_path)
+    
+    X_train, X_test, y_train, y_test = train_test_split(
+        data.drop("loan_status", axis=1),
+        data["loan_status"],
+        random_state=42,
+        test_size=0.2)
+    
+    input_example = X_train[0:5]
+    n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 505
+    max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 37
+    
+    with mlflow.start_run():
+        model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
+        model.fit(X_train, y_train)
+        
+        predicted_qualities = model.predict(X_test)
+        
+        mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path="model",
+            input_example=input_example)
+        
+        model.fit(X_train, y_train)
+        
+        # Log metrics
+        accuracy = model.score(X_test, y_test)
+        mlflow.log_metric("accuracy", accuracy)

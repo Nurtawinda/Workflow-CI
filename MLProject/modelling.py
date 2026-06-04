ab1fc6ac7884aaf9fc6d57b90e97c3f1008e1cc0
WinWind <101435968+Nurtawinda@users.noreply.github.com>
2026-06-04 17:14:32 +0800

    Refactor ML model training and logging process

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -17,13 +17,13 @@ if __name__ == "__main__":
         os.environ["MLFLOW_TRACKING_USERNAME"] = "Nurtawinda"
         os.environ["MLFLOW_TRACKING_PASSWORD"] = "e6481f90867f23522774600ba572268d89071390"
         mlflow.set_tracking_uri("https://dagshub.com")
-    
-    mlflow.set_experiment("Submission SML winda")
+        
+        mlflow.set_experiment("Submission SML winda")
     
     n_estimators = 505
     max_depth = 37
     
-    # 2. Deteksi Otomatis Jalur Dataset
+    # 2. Deteksi Jalur Dataset
     base_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(base_dir, "credit_risk_dataset_preprocessing.csv")
     
@@ -43,25 +43,24 @@ if __name__ == "__main__":
     input_example = X_train[0:5]
     
     # 3. Proses Training Model
-    with mlflow.start_run(run_name=f"MLProject_est_{n_estimators}_depth_{max_depth}", nested=True):
-        mlflow.autolog(disable=True)
+    mlflow.autolog(disable=True)
 
-        model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, n_jobs=-1, random_state=42)
-        model.fit(X_train, y_train)
+    model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, n_jobs=-1, random_state=42)
+    model.fit(X_train, y_train)
+    
+    # Simpan ke log artefak internal
+    mlflow.sklearn.log_model(sk_model=model, artifact_path="model", input_example=input_example)
+    
+    # Log Metrik Akurasi
+    accuracy = model.score(X_test, y_test)
+    mlflow.log_metric("accuracy", accuracy)
+    
+    # 4. Ambil folder model fisik lokal ke dalam folder MLProject/model
+    local_model_path = os.path.join(base_dir, "model")
+    if os.path.exists(local_model_path):
+        shutil.rmtree(local_model_path)
         
-        # Simpan ke log artefak internal
-        mlflow.sklearn.log_model(sk_model=model, artifact_path="model", input_example=input_example)
-        
-        # Log Metrik Akurasi
-        accuracy = model.score(X_test, y_test)
-        mlflow.log_metric("accuracy", accuracy)
-        
-        # 4. Ambil folder model fisik lokal ke dalam folder MLProject/model
-        local_model_path = os.path.join(base_dir, "model")
-        if os.path.exists(local_model_path):
-            shutil.rmtree(local_model_path)
-            
-        mlflow.sklearn.save_model(sk_model=model, path=local_model_path)
-        print(f"-> Sukses! Model fisik lokal disimpan di: {local_model_path}")
+    mlflow.sklearn.save_model(sk_model=model, path=local_model_path)
+    print(f"-> Sukses! Model fisik lokal disimpan di: {local_model_path}")
 
     print(f"-> Selesai Sempurna! Akurasi Model Final: {accuracy:.4f}")
